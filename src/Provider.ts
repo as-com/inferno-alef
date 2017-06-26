314e168e6f631a9f30d9a05e3a8851bf8029be37
Andrew Sun <me@andrewsun.com>
2017-06-26 16:20:37 -0400

    Move inferno-alef into own repository

---
+++ src/Provider.ts
@@ -0,0 +1,23 @@
+import Component from "inferno-component";
+import {render} from "alef-dom";
+
+import {DOMRenderer} from "../../../src/types/DOMRenderer";
+
+export interface IProviderProps {
+	renderer: DOMRenderer;
+
+}
+
+export default class Provider extends Component<IProviderProps, {}> {
+	getChildContext() {
+		return { renderer: this.props.renderer };
+	}
+
+	componentDidMount() {
+		render(this.props.renderer);
+	}
+
+	render() {
+		return this.props.children;
+	}
+}
